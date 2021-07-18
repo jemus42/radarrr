e29673262e57f4d354c47abe97e4ddccf82ef81e
Lukas Burk <lukas@quantenbrot.de>
2021-07-18 03:28:43 +0200

    More small functions

--- NEWS.md
+++ NEWS.md
@@ -2,6 +2,6 @@
 
 * Switch to API v3.
 * Housekeeping: pkgdown, styler, usethis, gh actions
-* Added `get_movies`
-* Added `system_status`
+* Added `get_movies`, `get_history`.
+* Added `system_status`, `update`, `root_folder`, `health`.
 * Added a `NEWS.md` file to track changes to the package.
--- R/system.R
+++ R/system.R
@@ -10,3 +10,51 @@
 system_status <- function() {
   radarr_get("/api/v3/system/status")
 }
+
+#' Get failed health checks
+#'
+#' @return a tibble.
+#' @export
+#'
+#' @examples
+#' \dontrun{
+#' health()
+#' }
+health <- function() {
+  radarr_get("/api/v3/health") %>%
+    as_tibble()
+}
+
+#' Get recent updates
+#'
+#' @return a tibble.
+#' @export
+#'
+#' @examples
+#' \dontrun{
+#' update()
+#' }
+update <- function() {
+  radarr_get("/api/v3/update") %>%
+    as_tibble()
+}
+
+#' Get root folders
+#'
+#' @return a tibble.
+#' @export
+#'
+#' @examples
+#' \dontrun{
+#' root_folder()
+#' }
+root_folder <- function() {
+  res <- radarr_get("/api/v3/rootfolder") %>%
+    as_tibble()
+
+  if (requireNamespace("tidyr", quietly = TRUE)) {
+    res <- tidyr::unchop(res, unmappedFolders)
+  }
+
+  res
+}
